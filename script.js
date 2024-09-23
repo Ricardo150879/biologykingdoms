d1a83fed8b30807d1841b40072420c0d2d9796ff
Ricardo150879 <ricardo.grasshopper@gmail.com>
2024-09-23 10:49:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+
+        function algorithmimga() {
+
+    var stra; 
+    var stra1="animalia"; 
+    var stra2="Animalia";
+    var stra3="ANIMALIA";	
+    stra=prompt('Enter the Biology Kingdom',' '); 
+    if (stra===stra1 || stra===stra2 || stra===stra3) { 
+alert("CONGRATULATIONS THE CORRECT ANSWER IS ANIMALIA");
+document.getElementById("IMGA").innerHTML = "<strong>" + "CONGRATULATIONS THE CORRECT ANSWER IS ANIMALIA " + "</strong>" + "&#x1F44D"; 
+  }
+    else { alert("IT ISEN'T THE CORRECT ANSWER. YOU CAN TRY IT AGAIN, OR MAYBE YOU FORGOT TO FILL OUT IN THE BLANK FIELD. PLEASE, TRY IT AGAIN"); }
+        
+      }
+
+        
+        function algorithmimgf() {
+
+    var strf; 
+    var strf1="fungi"; 
+    var strf2="Fungi";
+    var strf3="FUNGI";	
+    strf=prompt('Enter the Biology Kingdom',' '); 
+    if (strf===strf1 || strf===strf2 || strf===strf3) { 
+alert("CONGRATULATIONS THE CORRECT ANSWER IS FUNGI");
+document.getElementById("IMGF").innerHTML = "<strong>" + "CONGRATULATIONS THE CORRECT ANSWER IS FUNGI " + "</strong>" + "&#x1F44D"; 
+  }
+    else { alert("IT ISEN'T THE CORRECT ANSWER. YOU CAN TRY IT AGAIN, OR MAYBE YOU FORGOT TO FILL OUT IN THE BLANK FIELD. PLEASE, TRY IT AGAIN"); }
+        
+      }
+
+        
+        function algorithmimgp() {
+
+    var strp; 
+    var strp1="plantae"; 
+    var strp2="Plantae";
+    var strp3="PLANTAE";	
+    strp=prompt('Enter the Biology Kingdom',' '); 
+    if (strp===strp1 || strp===strp2 || strp===strp3) { 
+alert("CONGRATULATIONS THE CORRECT ANSWER IS PLANTAE");
+document.getElementById("IMGP").innerHTML = "<strong>" + "CONGRATULATIONS THE CORRECT ANSWER IS PLANTAE " + "</strong>" + "&#x1F44D"; 
+  }
+    else { alert("IT ISEN'T THE CORRECT ANSWER. YOU CAN TRY IT AGAIN, OR MAYBE YOU FORGOT TO FILL OUT IN THE BLANK FIELD. PLEASE, TRY IT AGAIN"); }
+        
+      }
+        
+        function algorithmimgc() {
+
+    var strc; 
+    var strc1="chromista"; 
+    var strc2="Chromista";
+    var strc3="CHROMISTA";	
+    strc=prompt('Enter the Biology Kingdom',' '); 
+    if (strc===strc1 || strc===strc2 || strc===strc3) { 
+alert("CONGRATULATIONS THE CORRECT ANSWER IS CHROMISTA");
+document.getElementById("IMGC").innerHTML = "<strong>" + "CONGRATULATIONS THE CORRECT ANSWER IS CHROMISTA " + "</strong>" + "&#x1F44D"; 
+  }
+    else { alert("IT ISEN'T THE CORRECT ANSWER. YOU CAN TRY IT AGAIN, OR MAYBE YOU FORGOT TO FILL OUT IN THE BLANK FIELD. PLEASE, TRY IT AGAIN"); }
+        
+      }
+        
+        function algorithmimgpr() {
+
+    var strpr; 
+    var strpr1="protozoa"; 
+    var strpr2="Protozoa";
+    var strpr3="PROTOZOA";	
+    strpr=prompt('Enter the Biology Kingdom',' '); 
+    if (strpr===strpr1 || strpr===strpr2 || strpr===strpr3) { 
+alert("CONGRATULATIONS THE CORRECT ANSWER IS PROTOZOA");
+document.getElementById("IMGPR").innerHTML = "<strong>" + "CONGRATULATIONS THE CORRECT ANSWER IS PROTOZOA" + "</strong>" + "&#x1F44D"; 
+  }
+    else { alert("IT ISEN'T THE CORRECT ANSWER. YOU CAN TRY IT AGAIN, OR MAYBE YOU FORGOT TO FILL OUT IN THE BLANK FIELD. PLEASE, TRY IT AGAIN"); }
+        
+      }
+        
+        function algorithmimgb() {
+
+    var strb; 
+    var strb1="bacteria"; 
+    var strb2="Bacteria";
+    var strb3="BACTERIA";	
+    strb=prompt('Enter the Biology Kingdom',' '); 
+    if (strb===strb1 || strb===strb2 || strb===strb3) { 
+alert("CONGRATULATIONS THE CORRECT ANSWER IS BACTERIA");
+document.getElementById("IMGB").innerHTML = "<strong>" + "CONGRATULATIONS THE CORRECT ANSWER IS BACTERIA" + "</strong>" + "&#x1F44D"; 
+  }
+    else { alert("IT ISEN'T THE CORRECT ANSWER. YOU CAN TRY IT AGAIN, OR MAYBE YOU FORGOT TO FILL OUT IN THE BLANK FIELD. PLEASE, TRY IT AGAIN"); }
+        
+      }
+
